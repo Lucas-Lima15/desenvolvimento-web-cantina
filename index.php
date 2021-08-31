e331a5e35ff5125861ad1ad15ff89ce01fa7eb12
Lucas Lima <lucasvinilima15@gmail.com>
2021-08-30 21:23:15 -0300

    alterando os elementos na navbar e adcionando css

--- index.php
+++ index.php
@@ -8,37 +8,42 @@
 
   <!-- CSS do Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  <!-- ================================================================================================================= -->
+  <link rel="stylesheet" href="css/index.css">
 
   <title>Cantina web</title>
 </head>
 
 <body>
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Cantina Web</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Objetivo</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Vantagens</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Sua Escola</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Equipe</a>
-          </li>
-        </ul>
+  <div class="container-fluid">
+    <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Cantina Web</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav" >
+            <li class="nav-item">
+              <a class="nav-link" href="#">Objetivo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Vantagens</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sua Escola</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Equipe</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+    <div>
 
+    </div>
+  </div>
   <!-- JS do Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
