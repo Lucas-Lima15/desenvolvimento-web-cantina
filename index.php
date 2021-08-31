04b606689b64e57b900257dfcbbaf6da34ae14e1
Lucas Lima <lucasvinilima15@gmail.com>
2021-08-30 23:09:51 -0300

    Adicionando a parte de Objetivos

--- index.php
+++ index.php
@@ -15,35 +15,51 @@
 </head>
 
 <body>
-  <div class="container-fluid">
-    <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light">
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Cantina Web</a>
+        <a class="navbar-brand" href="#navbarNav">
+          <h1>Cantina Web</h1>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul class="navbar-nav" >
+          <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="#">Objetivo</a>
+              <a class="nav-link" href="#objetivos">Objetivo</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Vantagens</a>
+              <a class="nav-link" href="#vantagens">Vantagens</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Sua Escola</a>
+              <a class="nav-link" href="#sua-escola">Sua Escola</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Equipe</a>
+              <a class="nav-link" href="#equipe">Equipe</a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-    <div>
+  </header>
 
+  <main>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-lg-4 ">
+          <h3>Objetivo 1</h3>
+        </div>
+        <div class="col-lg-4 ">
+          <h3>Objetivo 2</h3>
+        </div>
+        <div class="col-lg-4 ">
+          <h3>Objetivo 3</h3>
+        </div>
+      </div>
     </div>
-  </div>
+  </main>
+
   <!-- JS do Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
