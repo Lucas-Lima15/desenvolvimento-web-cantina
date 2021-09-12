d426573446851d06c04073530d366a61ff5b9a0a
Lucas-Lima15 <lucasvinilima15@gmail.com>
2021-09-12 20:16:15 -0300

    adcionando formulario

--- login.php
+++ login.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+    <main class="container text-center">
+        <form action="">
+            <div class="mb-3">
+                <label for="email-input" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email-input">
+            </div>
+            <div class="mb-3">
+                <label for="senha-input" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="senha-input">
+            </div>
+            <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
+
 </html>
