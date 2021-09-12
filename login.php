6f38aa1889a11df56be22af401c7409b78b87d71
Lucas-Lima15 <lucasvinilima15@gmail.com>
2021-09-12 20:32:17 -0300

    Acrescentando css e linkando com idex.php

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
     <title>Document</title>
 </head>
 
@@ -13,13 +14,13 @@
 
     <main class="container text-center">
         <form action="">
-            <div class="mb-3">
+            <div class="mb-3 lg-2">
                 <label for="email-input" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email-input">
+                <input type="email" class="form-control" id="email-input" required>
             </div>
-            <div class="mb-3">
+            <div class="mb-3 lg-2">
                 <label for="senha-input" class="form-label">Senha</label>
-                <input type="password" class="form-control" id="senha-input">
+                <input type="password" class="form-control" id="senha-input" required> 
             </div>
             <button type="submit" class="btn btn-primary">Entrar</button>
         </form>
